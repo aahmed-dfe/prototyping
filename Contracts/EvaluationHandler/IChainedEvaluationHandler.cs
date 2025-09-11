6fae6eee7e02be924ec964bc87f3e2590cc83e39
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2025-09-11 09:11:01 +0100

    chained contract

---
+++ Contracts/EvaluationHandler/IChainedEvaluationHandler.cs
@@ -0,0 +1,5 @@
+﻿namespace Contracts.EvaluationHandler;
+public interface IChainedEvaluationHandler<TInput, TOutput> : IEvaluationHandler<TInput, TOutput>
+{
+    public IEvaluationHandler<TInput, TOutput> ChainNext(IEvaluationHandler<TInput, TOutput> handler);
+}
