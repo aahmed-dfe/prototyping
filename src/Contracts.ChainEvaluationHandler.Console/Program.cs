17638c72d971873fd0b77c2159cbafd261a7449c
aahmed-dfe <aasim.ahmed@education.gov.uk>
2025-09-19 09:39:47 +0100

    test project

--- src/Contracts.ChainEvaluationHandler.Console/Program.cs
+++ src/Contracts.ChainEvaluationHandler.Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 using Contracts.ChainEvaluationHandler.EvaluationHandler;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
